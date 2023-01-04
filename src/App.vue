e4ef7e6fbc370c1cf9d57db2db920a3f70d39a3f
FabioDevCode <0677384849@orange.fr>
2023-01-04 19:37:20 +0100

    First commit 2

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<h1>TEST</h1>
+</template>
+
+
+<script>
+	export default {
+		name: 'VueTodo',
+		data() {
+			return {
+
+			}
+		},
+		mounted() {
+
+
+		},
+		methods: {
+
+
+		}
+	}
+</script>
+
+
+<style>
+
+</style>
