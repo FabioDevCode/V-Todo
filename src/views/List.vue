af336923df2a08e6b1138e0ba133e474004329a3
FabioDevCode <0677384849@orange.fr>
2023-01-08 18:29:30 +0100

    Add feature to delete an task in list

--- src/views/List.vue
+++ src/views/List.vue
@@ -3,8 +3,8 @@
     <SubTitle :title="'Ajout de tâche à '+ actualList.name" v-if="showModal" />
 
     <ul id="list" v-if="hideList">
-        <li @click="endedTask(actualList.tasks.indexOf(task), task.title)"  :key="task.name" v-for="task in actualList.tasks" :class="'task ' + task.status">
-            <div class="text">
+        <li :key="task.name" v-for="task in actualList.tasks" :class="'task ' + task.status">
+            <div class="text" @click="endedTask(actualList.tasks.indexOf(task), task.title)">
                 <h3>
                     {{ task.title }}
                 </h3>
@@ -12,8 +12,8 @@
                     {{ task.desc }}
                 </p>
             </div>
-            <div class="trash">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
+            <div class="trash" @click="supprTask(actualList.tasks.indexOf(task), task.title)">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M160 400C160 408.8 152.8 416 144 416C135.2 416 128 408.8 128 400V192C128 183.2 135.2 176 144 176C152.8 176 160 183.2 160 192V400zM240 400C240 408.8 232.8 416 224 416C215.2 416 208 408.8 208 400V192C208 183.2 215.2 176 224 176C232.8 176 240 183.2 240 192V400zM320 400C320 408.8 312.8 416 304 416C295.2 416 288 408.8 288 400V192C288 183.2 295.2 176 304 176C312.8 176 320 183.2 320 192V400zM317.5 24.94L354.2 80H424C437.3 80 448 90.75 448 104C448 117.3 437.3 128 424 128H416V432C416 476.2 380.2 512 336 512H112C67.82 512 32 476.2 32 432V128H24C10.75 128 0 117.3 0 104C0 90.75 10.75 80 24 80H93.82L130.5 24.94C140.9 9.357 158.4 0 177.1 0H270.9C289.6 0 307.1 9.358 317.5 24.94H317.5zM151.5 80H296.5L277.5 51.56C276 49.34 273.5 48 270.9 48H177.1C174.5 48 171.1 49.34 170.5 51.56L151.5 80zM80 432C80 449.7 94.33 464 112 464H336C353.7 464 368 449.7 368 432V128H80V432z"/></svg>
             </div>
         </li>
     </ul>
@@ -169,6 +169,25 @@
                         }
                     }).showToast();
             },
+            supprTask(indexTask, titleTask) {
+                const allTasks = document.querySelectorAll('.task')[indexTask];
+                allTasks.classList.add('none');
+
+                this.actualList.tasks.splice(indexTask, 1);
+
+                const all_data = JSON.parse(localStorage.getItem('f-devcode_v-todo'));
+                all_data.list.map(el => {
+                    if(el.name === this.list_name){
+                        el.tasks = this.actualList.tasks
+                    }
+                    return el
+                });
+
+                localStorage.setItem('f-devcode_v-todo', JSON.stringify(all_data));
+
+
+                // Delete task on list
+            },
             endedTask(indexTask, titleTask) {
                 const allTasks = document.querySelectorAll('.task')[indexTask];
                 allTasks.classList.toggle('todo');
@@ -197,7 +216,7 @@
                     return el
                 });
 
-                localStorage.setItem('f-devcode_v-todo', JSON.stringify(all_data));;
+                localStorage.setItem('f-devcode_v-todo', JSON.stringify(all_data));
             },
             deleteList() {
                 const all_data = JSON.parse(localStorage.getItem('f-devcode_v-todo'));
@@ -336,7 +355,6 @@
     }
 
     .btn_addtask {
-        background-color: crimson;
         margin: 0 auto 15px auto !important;
     }
 
@@ -431,6 +449,11 @@
         min-width: 200px;
         max-width: 300px;
         margin: 50px auto;
+    }
+
+    .none {
+        display: none !important;
+        visibility: hidden !important;
     }
 
 </style>
